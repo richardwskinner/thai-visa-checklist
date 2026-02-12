9ea4790bcbfbb2144e6153da8f0527d3b276a369
Richard Skinner <richardskinnermail@gmail.com>
2026-02-12 23:33:07 +0700

    Add extentkion of stay

--- lib/data/visas.ts
+++ lib/data/visas.ts
@@ -13,7 +13,7 @@ export const VISAS: VisaInfo[] = [
     slug: "marriage",
     emoji: "💑",
     title: "Marriage Visa",
-    subtitle: 'Non-Immigrant Type "O" (Marriage to a Thai National)',
+    subtitle: 'Non-Immigrant Type "O" (Extension of Stay Based on Marriage to a Thai National)',
     bg: "bg-pink-600",
     href: "/visa/marriage",
   },
