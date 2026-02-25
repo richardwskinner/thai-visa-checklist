4e57a0a4afb782ea5a2642a0120de6ac9c8a2d30
Richard Skinner <richardskinnermail@gmail.com>
2026-02-25 11:14:18 +0700

    mobile logo print fix

--- components/print-checklist-header.tsx
+++ components/print-checklist-header.tsx
@@ -1,17 +1,16 @@
-import Image from "next/image";
-
 export default function PrintChecklistHeader() {
   return (
     <div className="hidden print:block print:-mt-4 print:mb-1">
       <div className="flex items-center justify-center pb-1">
         <div className="flex items-center">
-          <Image
+          <img
             src="/logo-full.svg"
             alt="Thai Visa Checklist"
             width={220}
             height={30}
+            loading="eager"
+            decoding="sync"
             className="h-auto w-[180px]"
-            priority={false}
           />
         </div>
       </div>
